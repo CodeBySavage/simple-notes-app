f3dcb09a9d7ceb51e0a32d2b42dbe36e4088700b
Matt <matthew.savage@myyahoo.com>
2026-03-28 19:56:20 -0400

    Add Dockerfile, entrypoint, and environment variables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u4ivaf6i2_(9@!zllmbs3&_*py@%5td-#%@2#ggu^no)l&i8$q'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
