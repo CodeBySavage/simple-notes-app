dc74a2f3ae1aae454bc366eca10d4e6f5c5bd65a
Matt <matthew.savage@myyahoo.com>
2026-03-28 21:07:12 -0400

    Switching from SQLite to Postgres with Docker Compose

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+echo "Waiting for postgres..."
+while ! nc -z db 5432; do
+  sleep 0.1
+done
+echo "Postgres is up!"
+
 python manage.py migrate --noinput
 exec gunicorn backend.wsgi:application --bind 0.0.0.0:8000
